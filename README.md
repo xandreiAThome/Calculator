b9a777149fce8c8f873c6bf6ad545dd884952677
Ellexandrei Esponilla <xandrei37@gmail.com>
2023-04-07 22:35:07 +0800

    moved functions to the bottom of the document

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Calculator
 
-### Live at: https://xandreiathome.github.io/Calculator/
+### Made in Vanilla JS, HTML, and CSS
+
+### <a href="https://xandreiathome.github.io/Calculator/">👉 Live DEMO</a>
--- app.js
+++ app.js
@@ -7,6 +7,7 @@
     4) Enters the equal operator
     4.1) If another arithmetic operator is entered, evaluate the first expression before evaluating the latter one
 
+    BEWARE OF SPAGHETTI CODE
  */
 
 let firstNum = "";
@@ -21,6 +22,107 @@ let evaluated = false;
 let consec = true;
 let percentOp = false;
 let squareRootOp = false;
+
+const lowerScreen = document.querySelector(".lower-screen");
+const upperScreen = document.querySelector(".upper-screen");
+
+// Records the typed numbers in the calculator
+const numbers = document.querySelectorAll(".numbers");
+numbers.forEach((n) => {
+  n.addEventListener("click", (e) => {
+    // IF already evaluated and there is no consecutive operator then just empty the screen
+    // checks first num is not empty because the answer from each evaluation is stored as the first num of a consecutive operation if there is any
+    if (evaluated && firstNum !== "" && !consec) {
+      upperScreen.innerHTML = "";
+      empty();
+      evaluated = false;
+    }
+    // If there is still no first operator
+    if (firstOp === "" || firstOp === "√") {
+      if (e.target.innerHTML === "." && firstDot) {
+        void 0;
+      } else {
+        firstNum += e.target.innerHTML;
+        lowerScreen.innerHTML = "";
+        lowerScreen.innerHTML = firstNum;
+
+        if (e.target.innerHTML === ".") firstDot = true;
+      }
+      // if there is now a first operator
+    } else if (firstOp !== "" && !percentOp && !squareRootOp) {
+      if (e.target.innerHTML === "." && secondDot) {
+        void 0;
+      } else {
+        secondNum += e.target.innerHTML;
+        lowerScreen.innerHTML = "";
+        lowerScreen.innerHTML = secondNum;
+
+        if (e.target.innerHTML === ".") secondDot = true;
+      }
+    }
+  });
+});
+
+const CE = document.getElementById("CE-btn");
+CE.addEventListener("click", (e) => {
+  lowerScreen.innerHTML = "";
+  upperScreen.innerHTML = "";
+  evaluated = false;
+  empty();
+});
+
+// Handles the input of operations
+const operations = document.querySelectorAll(".operations");
+operations.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    // if first number is entered and there is still no first operator and the first operator is not an equal sign
+    // Or if the first operator is a squareroot
+    if (
+      (e.target.innerHTML === "√" || firstNum !== "") &&
+      firstOp === "" &&
+      e.target.innerHTML !== "="
+    ) {
+      // if first operator is squareroot
+      if (e.target.innerHTML === "√") {
+        squareRootOp = true;
+        firstOp = e.target.innerHTML;
+        upperScreen.innerHTML = "";
+        upperScreen.innerHTML = firstOp + firstNum;
+      } else {
+        firstOp = e.target.innerHTML;
+        upperScreen.innerHTML = "";
+        upperScreen.innerHTML = firstNum + firstOp;
+      }
+      // if evaluated already and inputted a operator right after
+      if (evaluated) consec = true;
+
+      if (e.target.innerHTML === "%") percentOp = true;
+
+      // if there is a first operator and second number entered
+    } else if (firstOp !== "" && secondNum !== "") {
+      secondOp = e.target.innerHTML;
+      upperScreen.innerHTML = "";
+      upperScreen.innerHTML = firstNum + firstOp + secondNum;
+      if (e.target.innerHTML === "=") {
+        evaluated = true;
+        consec = false;
+      }
+      evaluate();
+    } else if (firstOp === "%" && firstNum !== "") {
+      secondOp = e.target.innerHTML;
+      upperScreen.innerHTML = "";
+      upperScreen.innerHTML = firstNum + firstOp;
+
+      evaluate();
+    } else if (firstNum !== "" && firstOp === "√") {
+      secondOp = e.target.innerHTML;
+      upperScreen.innerHTML = "";
+      upperScreen.innerHTML = firstOp + firstNum;
+
+      evaluate();
+    }
+  });
+});
 
 function evaluate() {
   switch (firstOp) {
@@ -120,103 +222,3 @@ function empty() {
   percentOp = false;
   squareRootOp = false;
 }
-
-const lowerScreen = document.querySelector(".lower-screen");
-const upperScreen = document.querySelector(".upper-screen");
-
-// Records the typed numbers in the calculator
-const numbers = document.querySelectorAll(".numbers");
-numbers.forEach((n) => {
-  n.addEventListener("click", (e) => {
-    // IF already evaluated and there is no consecutive operator then just empty the screen
-    // checks first num is not empty because the answer from each evaluation is stored as the first num of a consecutive operation if there is any
-    if (evaluated && firstNum !== "" && !consec) {
-      upperScreen.innerHTML = "";
-      empty();
-      evaluated = false;
-    }
-    // If there is still no first operator
-    if (firstOp === "" || firstOp === "√") {
-      if (e.target.innerHTML === "." && firstDot) {
-        void 0;
-      } else {
-        firstNum += e.target.innerHTML;
-        lowerScreen.innerHTML = "";
-        lowerScreen.innerHTML = firstNum;
-
-        if (e.target.innerHTML === ".") firstDot = true;
-      }
-      // if there is now a first operator
-    } else if (firstOp !== "" && !percentOp && !squareRootOp) {
-      if (e.target.innerHTML === "." && secondDot) {
-        void 0;
-      } else {
-        secondNum += e.target.innerHTML;
-        lowerScreen.innerHTML = "";
-        lowerScreen.innerHTML = secondNum;
-
-        if (e.target.innerHTML === ".") secondDot = true;
-      }
-    }
-  });
-});
-
-const CE = document.getElementById("CE-btn");
-CE.addEventListener("click", (e) => {
-  lowerScreen.innerHTML = "";
-  upperScreen.innerHTML = "";
-  evaluated = false;
-  empty();
-});
-
-// Handles the input of operations
-const operations = document.querySelectorAll(".operations");
-operations.forEach((op) => {
-  op.addEventListener("click", (e) => {
-    // if first number is entered and there is still no first operator and the first operator is not an equal sign
-    if (
-      (e.target.innerHTML === "√" || firstNum !== "") &&
-      firstOp === "" &&
-      e.target.innerHTML !== "="
-    ) {
-      //
-      if (e.target.innerHTML === "√") {
-        squareRootOp = true;
-        firstOp = e.target.innerHTML;
-        upperScreen.innerHTML = "";
-        upperScreen.innerHTML = firstOp + firstNum;
-      } else {
-        firstOp = e.target.innerHTML;
-        upperScreen.innerHTML = "";
-        upperScreen.innerHTML = firstNum + firstOp;
-      }
-      // if evaluated already and inputted a operator right after
-      if (evaluated) consec = true;
-
-      if (e.target.innerHTML === "%") percentOp = true;
-
-      // if there is a first operator and second number entered
-    } else if (firstOp !== "" && secondNum !== "") {
-      secondOp = e.target.innerHTML;
-      upperScreen.innerHTML = "";
-      upperScreen.innerHTML = firstNum + firstOp + secondNum;
-      if (e.target.innerHTML === "=") {
-        evaluated = true;
-        consec = false;
-      }
-      evaluate();
-    } else if (firstOp === "%" && firstNum !== "") {
-      secondOp = e.target.innerHTML;
-      upperScreen.innerHTML = "";
-      upperScreen.innerHTML = firstNum + firstOp;
-
-      evaluate();
-    } else if (firstNum !== "" && firstOp === "√") {
-      secondOp = e.target.innerHTML;
-      upperScreen.innerHTML = "";
-      upperScreen.innerHTML = firstOp + firstNum;
-
-      evaluate();
-    }
-  });
-});
