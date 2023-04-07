8b38fa0239d58e48c026c58e1ae3e39a7c2825d2
Ellexandrei Esponilla <xandrei37@gmail.com>
2023-04-07 15:29:27 +0800

    refactored the logic of evaluating expressions and consecutive expressions

--- app.js
+++ app.js
@@ -1,31 +1,82 @@
-let operator = [];
+/**
+    Calculator Pseudo Code
+    
+    1) Enters the first number
+    2) Enters the arithmetic operation
+    3) Enters the second number
+    4) Enters the equal operator
+    4.1) If another arithmetic operator is entered, evaluate the first expression before evaluating the latter one
+
+ */
+
 let firstNum = "";
+let firstDot = false;
+let firstOp = "";
+
 let secondNum = "";
-let ans = "";
-let consec = false;
+let secondDot = false;
+let secondOp = "";
 
 function evaluate() {
-  switch (operator[0]) {
+  switch (firstOp) {
     case "+":
       add(firstNum, secondNum);
-      empty();
+      break;
+    case "-":
+      subtract(firstNum, secondNum);
+      break;
+    case "×":
+      multiply(firstNum, secondNum);
+      break;
+    case "÷":
+      divide(firstNum, secondNum);
       break;
   }
 }
 
 function add(numA, numB) {
-  let sum = parseFloat(numA) + parseFloat(numB);
+  let ans = parseFloat(numA) + parseFloat(numB);
+  // if the second operator is not an equal sign, store the operator and make it the first operator for the subsequent expressions
+  let lastOp = secondOp;
   lowerScreen.innerHTML = "";
-  lowerScreen.innerHTML = sum;
-  ans = String(sum);
-  consec = true;
+  lowerScreen.innerHTML = ans;
+  empty();
+  firstNum = String(ans);
+  if (secondOp !== "=") firstOp = lastOp;
 }
 
-function subtract() {}
+function subtract(numA, numB) {
+  let ans = parseFloat(numA) - parseFloat(numB);
+  // if the second operator is not an equal sign, store the operator and make it the first operator for the subsequent expressions
+  let lastOp = secondOp;
+  lowerScreen.innerHTML = "";
+  lowerScreen.innerHTML = ans;
+  empty();
+  firstNum = String(ans);
+  if (secondOp !== "=") firstOp = lastOp;
+}
 
-function multiply() {}
+function multiply(numA, numB) {
+  let ans = parseFloat(numA) * parseFloat(numB);
+  // if the second operator is not an equal sign, store the operator and make it the first operator for the subsequent expressions
+  let lastOp = secondOp;
+  lowerScreen.innerHTML = "";
+  lowerScreen.innerHTML = ans;
+  empty();
+  firstNum = String(ans);
+  if (secondOp !== "=") firstOp = lastOp;
+}
 
-function divide() {}
+function divide(numA, numB) {
+  let ans = parseFloat(numA) / parseFloat(numB);
+  // if the second operator is not an equal sign, store the operator and make it the first operator for the subsequent expressions
+  let lastOp = secondOp;
+  lowerScreen.innerHTML = "";
+  lowerScreen.innerHTML = ans;
+  empty();
+  firstNum = String(ans);
+  if (secondOp !== "=") firstOp = lastOp;
+}
 
 function percentage() {}
 
@@ -33,9 +84,12 @@ function squareRoot() {}
 
 // empties varianbles
 function empty() {
-  operator = [];
   firstNum = "";
   secondNum = "";
+  firstOp = "";
+  secondOp = "";
+  firstDot = false;
+  secondDot = false;
 }
 
 const lowerScreen = document.querySelector(".lower-screen");
@@ -45,20 +99,29 @@ const upperScreen = document.querySelector(".upper-screen");
 const numbers = document.querySelectorAll(".numbers");
 numbers.forEach((n) => {
   n.addEventListener("click", (e) => {
-    if (consec) {
-      upperScreen.innerHTML = "";
-      lowerScreen.innerHTML = "";
-      consec = false;
-    }
+    // If there is still no first operator
+    if (firstOp === "") {
+      console.log("HI");
+      if (e.target.innerHTML === "." && firstDot) {
+        void 0;
+      } else {
+        firstNum += e.target.innerHTML;
+        lowerScreen.innerHTML = "";
+        lowerScreen.innerHTML = firstNum;
 
-    if (operator.length === 0) {
-      firstNum += e.target.innerHTML;
-      lowerScreen.innerHTML = "";
-      lowerScreen.innerHTML = firstNum;
-    } else {
-      secondNum += e.target.innerHTML;
-      lowerScreen.innerHTML = "";
-      lowerScreen.innerHTML = secondNum;
+        if (e.target.innerHTML === ".") firstDot = true;
+      }
+      // if there is now a first operator
+    } else if (firstOp !== "") {
+      if (e.target.innerHTML === "." && secondDot) {
+        void 0;
+      } else {
+        secondNum += e.target.innerHTML;
+        lowerScreen.innerHTML = "";
+        lowerScreen.innerHTML = secondNum;
+
+        if (e.target.innerHTML === ".") secondDot = true;
+      }
     }
   });
 });
@@ -74,22 +137,17 @@ CE.addEventListener("click", (e) => {
 const operations = document.querySelectorAll(".operations");
 operations.forEach((op) => {
   op.addEventListener("click", (e) => {
-    if (operator.length <= 2) {
-      if (firstNum.length !== 0) {
-        // if there is a typed number
-        if (e.target.innerHTML !== "=") {
-          let op = document.createTextNode(firstNum + e.target.innerHTML);
-          upperScreen.appendChild(op);
-        }
-
-        operator.push(e.target.innerHTML);
-
-        if (operator.length === 2 && operator[1] === "=") {
-          let sec = document.createTextNode(secondNum);
-          upperScreen.appendChild(sec);
-          evaluate();
-        }
-      }
+    // if first number is entered and there is still no first operator
+    if (firstNum !== "" && firstOp === "") {
+      firstOp = e.target.innerHTML;
+      upperScreen.innerHTML = "";
+      upperScreen.innerHTML = firstNum + firstOp;
+      // if there is a first operator and second number entered
+    } else if (firstOp !== "" && secondNum !== "") {
+      secondOp = e.target.innerHTML;
+      upperScreen.innerHTML = "";
+      upperScreen.innerHTML = firstNum + firstOp + secondNum;
+      evaluate();
     }
   });
 });
